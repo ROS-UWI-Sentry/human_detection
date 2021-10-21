604881ca8df727b6edaebda89cd9ee8ec20de646
Ian Sooknanan <ian.sooknanan@my.uwi.edu>
2021-10-21 17:45:30 -0400

    minor imrovements for launching with state machine

--- launch/run_script.sh
+++ launch/run_script.sh
@@ -5,7 +5,7 @@ cd yolov5
 
 ls -ltrh /dev/video* > streams.txt
 
-python2 get_devices_ros.py
+python2 get_devices.py
 
 python3 detect.py --source streams.txt --classes 0 --nosave --weights yolov5l6.pt --webcamDebounceFrames 5
 
